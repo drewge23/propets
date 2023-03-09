685132326cdbab0e7099d0aec2c7e14063daa5d5
drewge23 <screwnut152@gmail.com>
2023-03-09 15:38:08 +0200

    Added sublinks to SideNav

--- src/components/content/sideNav/SideNav.jsx
+++ src/components/content/sideNav/SideNav.jsx
@@ -1,45 +1,79 @@
 import React, {useState} from 'react';
-import {Link} from "react-router-dom";
 import s from './sideNav.module.css'
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faBullhorn, faHome, faPaw, faSearch, faStar} from '@fortawesome/free-solid-svg-icons'
+import {
+    faBullhorn,
+    faDog,
+    faHome,
+    faHotel,
+    faPaw,
+    faPersonWalking,
+    faSearch,
+    faStar, faStethoscope
+} from '@fortawesome/free-solid-svg-icons'
 import SideNavLink from "./SideNavLink";
 
 function SideNav(props) {
     const [active, setActive] = useState(0)
 
-    const linkArray = [{
-        to: 'home',
-        icon: <FontAwesomeIcon icon={faHome}/>,
-        text: 'Home',
-        state: null,
-    }, {
-        to: 'lost&found',
-        icon: <FontAwesomeIcon icon={faSearch}/>,
-        text: 'Lost',
-        state: {isLost: true},
-    }, {
-        to: 'lost&found',
-        icon: <FontAwesomeIcon icon={faPaw}/>,
-        text: 'Found',
-        state: {isLost: false},
-    }, {
-        to: 'services',
-        icon: <FontAwesomeIcon icon={faBullhorn}/>,
-        text: 'Services',
-        state: null,
-    }, {
-        to: 'favorites',
-        icon: <FontAwesomeIcon icon={faStar}/>,
-        text: 'Favorites',
-        state: null,
-    }]
+    const servicesChildren = [
+        {
+            to: 'hotels',
+            icon: <FontAwesomeIcon icon={faHotel}/>,
+            text: 'Hotels',
+        }, {
+            to: 'fostering',
+            icon: <FontAwesomeIcon icon={faPersonWalking}/>,
+            text: 'Fostering',
+        }, {
+            to: 'walking',
+            icon: <FontAwesomeIcon icon={faDog}/>,
+            text: 'Walking',
+        }, {
+            to: 'vethelp',
+            icon: <FontAwesomeIcon icon={faStethoscope}/>,
+            text: 'Vet Help',
+        },]
+
+    const linkArray = [
+        {
+            to: 'home',
+            icon: <FontAwesomeIcon icon={faHome}/>,
+            text: 'Home',
+            state: null,
+            children: null,
+        }, {
+            to: 'lost&found',
+            icon: <FontAwesomeIcon icon={faSearch}/>,
+            text: 'Lost',
+            state: {isLost: true},
+            children: null,
+        }, {
+            to: 'lost&found',
+            icon: <FontAwesomeIcon icon={faPaw}/>,
+            text: 'Found',
+            state: {isLost: false},
+            children: null,
+        }, {
+            to: 'services',
+            icon: <FontAwesomeIcon icon={faBullhorn}/>,
+            text: 'Services',
+            state: null,
+            children: servicesChildren,
+        }, {
+            to: 'favorites',
+            icon: <FontAwesomeIcon icon={faStar}/>,
+            text: 'Favorites',
+            state: null,
+            children: null,
+        }]
 
     return (
         <nav className={s.sideNav}>
-            {linkArray.map((link,index) => (
-                <SideNavLink {...link} isActive={index === active}/>
+            {linkArray.map((link, index) => (
+                <SideNavLink {...link} isActive={index === active}
+                             key={index} setActive={setActive} index={index}/>
             ))}
         </nav>
     );
--- src/components/content/sideNav/SideNavLink.jsx
+++ src/components/content/sideNav/SideNavLink.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from "./sideNav.module.css";
 import {Link} from "react-router-dom";
 
-function SideNavLink({to, icon, text, state, isActive}) {
+function SideNavLink({to, icon, text, state, isActive, setActive, index, children}) {
+    const [subActive, setSubActive] = useState(0)
+
     return (
-        <p className={isActive ? s.active : undefined}>
-            <Link to={to} state={state}>
+        <div className={(children && isActive) ? s.hasChildren : undefined}>
+            <div onClick={() => setActive(index)}
+                 className={isActive ? s.activeContainer : undefined}>
+                <p className={isActive ? s.active : undefined}>
+                    <Link to={to} state={state}>
                     <span className={s.icon}>
                         {icon}
                     </span> {text}
-            </Link>
-        </p>
+                    </Link>
+                </p>
+            </div>
+            {children && isActive && children.map((child, index) =>
+                (<p className={index === subActive ? s.subActive : undefined}
+                    onClick={() => setSubActive(index)}>
+                    <Link to={child.to}>
+                    <span className={s.icon}>
+                        {child.icon}
+                    </span> {child.text}
+                    </Link>
+                </p>))}
+        </div>
     );
 }
 
