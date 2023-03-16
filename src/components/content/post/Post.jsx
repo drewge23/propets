4bb39aaaa56de6cf116da86968176a9b53726c4a
Polina Binus <polibinus@gmail.com>
2023-03-16 20:03:33 +0200

    design

--- src/components/content/post/Post.jsx
+++ src/components/content/post/Post.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import s from './post.module.css'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faStar} from '@fortawesome/free-regular-svg-icons'
+
 
 function Post() {
     return (
         <div className={s.post}>
             <img src={'fromDB'} alt={'avatar'} className={s.avatar}/>
-            <div className={s.main}>
-                <h3 className={s.name}>John GoodBoy</h3>
+            <div className={s.name}>
+                <h3>John GoodBoy</h3>
                 <span className={s.time}>time</span>
+            </div>
+            <div className={s.main}>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, velit.</p>
                 <button className={s.more}>...more</button>
             </div>
-            <div>
-                <button className={s.menu}>...</button>
-                <div className={s.settings}>
-                    <button>
-                        Hide from feed
-                    </button>
-                    <button>
-                        Unfollow
-                    </button>
-                </div>
-                <button className={s.fav}>fav</button>
+            <span className={s.menu} onClick={'#'}>•••</span>
+            <div className={s.settings}>
+                <button>
+                    Hide from feed
+                </button>
+                <button>
+                    Unfollow
+                </button>
             </div>
+            <button className={s.fav}>
+                <FontAwesomeIcon icon={faStar} />
+            </button>
         </div>
     );
 }
