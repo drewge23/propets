9864852f50f777fe83bbe8a478d68576da8922e4
drewge23 <screwnut152@gmail.com>
2023-03-20 23:40:12 +0200

    Created Activities.jsx Edited LostFound section

---
+++ src/components/content/profile/Activities.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {useCollection} from "react-firebase-hooks/firestore";
+import {auth, db} from "../../../firebaseConfig";
+import LostFoundPost from "../LostFound/LostFoundPost";
+import {doc, deleteDoc} from "firebase/firestore";
+
+function Activities(props) {
+    const [activities] = useCollection(db.collection('lost_and_found')
+        .where('userId', '==', auth.currentUser.uid))
+
+    const deleteActivity = (postId) => {
+        deleteDoc(doc(db, 'lost_and_found', postId))
+    }
+
+    return (
+        <div>
+            <h2>Activities</h2>
+            {activities && activities.docs.map(post => (
+                <div key={post.id}>
+                    {post.createdAt?.seconds}
+                    <LostFoundPost post={post.data()}
+                                   postId={post.id}
+                                   editable
+                                   deleteActivity={deleteActivity}/>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Activities;
