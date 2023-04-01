ae9a5894a5dc2f6eb93afba9749dead2656758d7
drewge23 <screwnut152@gmail.com>
2023-04-01 17:26:59 +0300

    Added filter to LostFound.jsx

--- src/components/content/LostFound/LostFound.jsx
+++ src/components/content/LostFound/LostFound.jsx
@@ -25,11 +25,29 @@ function LostFound(props) {
     //     console.log(locations)
     // },[posts])
 
+    const [type, setType] = useState('')
+    const [breed, setBreed] = useState('')
+    const [features, setFeatures] = useState('')
+    const [place, setPlace] = useState('')
+
+    const filteredPosts = posts
+        ? posts.docs
+            .filter(post => {
+                return type === 'Any'
+                    ? true
+                    : post.data().type?.includes(type.trim())
+            })
+            .filter(post => post.data().breed.includes(breed.trim()))
+            .filter(post => post.data().distinctive_features.includes(features.trim()))
+            .filter(post => post.data().location.includes(place.trim()))
+        : undefined
+
     return (
         <div>
             <div className={s.filters}>
-                <select name={'Type'}>
-                    <option>Type</option>
+                <select name={'Type'} value={type}
+                        onChange={(e) => setType(e.target.value)}>
+                    <option>Any</option>
                     <option>Dog</option>
                     <option>Cat</option>
                     <option>Bunny</option>
@@ -37,15 +55,18 @@ function LostFound(props) {
                     <option>Parrot</option>
                     <option>Other</option>
                 </select>
-                <input placeholder={'Breed'}/>
-                <input placeholder={'Distinctive features'}/>
-                <input placeholder={'Location'}/>
+                <input placeholder={'Breed'}
+                       value={breed} onChange={(e) => setBreed(e.target.value)}/>
+                <input placeholder={'Distinctive features'}
+                       value={features} onChange={(e) => setFeatures(e.target.value)}/>
+                <input placeholder={'Location'}
+                       value={place} onChange={(e) => setPlace(e.target.value)}/>
             </div>
             <div className={s.main}>
                 <div>
-                    {posts && posts.docs
-                    	.sort((a, b) => b.data().createdAt?.seconds - a.data().createdAt?.seconds)
-                    	.map(post => <LostFoundPost post={post.data()}
+                    {posts && filteredPosts && filteredPosts
+                        .sort((a, b) => b.data().createdAt?.seconds - a.data().createdAt?.seconds)
+                        .map(post => <LostFoundPost post={post.data()}
                                                     postId={post.id}
                                                     key={post.id}/>)}
                 </div>
