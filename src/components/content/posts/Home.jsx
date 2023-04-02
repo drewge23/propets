a53a534caeff3594e40f236bc564f1631cb54eb3
Polina Binus <polibinus@gmail.com>
2023-04-02 12:14:59 +0300

    for all services

--- src/components/content/posts/Home.jsx
+++ src/components/content/posts/Home.jsx
@@ -4,45 +4,15 @@ import {useCollection, useDocument} from "react-firebase-hooks/firestore";
 import {auth, db} from "../../../firebaseConfig";
 
 function Home() {
-    const [posts] = useCollection(db.collection('posts').where('type', '==', 'home'))
-
-    const currentUserId = auth.currentUser.uid
-    const [subsPostIds, loadingSubs] = useDocument(db.collection('subscriptions').doc(currentUserId))
-    const [hiddenPostIds, setHiddenPostIds] = useState([])
-    const [unfollowedUserIds, setUnfollowedUserIds] = useState([])
-    const [displayedPosts, setDisplayedPosts] = useState({docs: []})
-
-    useEffect(() => {
-        if (subsPostIds) {
-            setHiddenPostIds(subsPostIds.data()?.hidden || [])
-            setUnfollowedUserIds(subsPostIds.data()?.unfollowed || [])
-        }
-    }, [subsPostIds])
-
-    useEffect(() => {
-        if (!posts) return
-        const temp = {docs: []}
-        temp.docs = posts.docs
-            .filter(doc => !unfollowedUserIds?.includes(doc.data().userId))
-            .filter(doc => !hiddenPostIds?.includes(doc.id))
-        setDisplayedPosts(temp)
-    }, [posts])
-
-    useEffect(() => {
-        const temp = {docs: []}
-        temp.docs = displayedPosts.docs
-            .filter(doc => !unfollowedUserIds?.includes(doc.data().userId))
-            .filter(doc => !hiddenPostIds?.includes(doc.id))
-        setDisplayedPosts(temp)
-    }, [hiddenPostIds, unfollowedUserIds])
+    const [posts, loading] = useCollection(db.collection('posts').where('type', '==', 'home'))
 
     return (
         <>
             {
-                loadingSubs
-                    ? <p>Loading...</p>
-                    : displayedPosts
-                        ? <Posts title={''} posts={displayedPosts}/>
+                loading
+                    ? <p>Paw...Paw...</p>
+                    : posts.docs.length
+                        ? <Posts title={''} posts={posts}/>
                         : <p>No posts</p>
             }
         </>
--- src/components/content/posts/Posts.jsx
+++ src/components/content/posts/Posts.jsx
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Post from "./Post";
+import {auth, db} from "../../../firebaseConfig";
+import {useDocument} from "react-firebase-hooks/firestore";
 
 function Posts({posts, type}) {
+
+    const currentUserId = auth.currentUser.uid
+    const [subsPostIds, loadingSubs] = useDocument(db.collection('subscriptions').doc(currentUserId))
+    const [hiddenPostIds, setHiddenPostIds] = useState([])
+    const [unfollowedUserIds, setUnfollowedUserIds] = useState([])
+    const [displayedPosts, setDisplayedPosts] = useState({docs: []})
+
+    useEffect(() => {
+        if (subsPostIds) {
+            setHiddenPostIds(subsPostIds.data()?.hidden || [])
+            setUnfollowedUserIds(subsPostIds.data()?.unfollowed || [])
+        }
+    }, [subsPostIds])
+
+    useEffect(() => {
+        if (!posts) return
+
+        const temp = {docs: []}
+        temp.docs = posts.docs
+            .filter(doc => !unfollowedUserIds?.includes(doc.data().userId))
+            .filter(doc => !hiddenPostIds?.includes(doc.id))
+
+        setDisplayedPosts(temp)
+    }, [posts])
+
+    useEffect(() => {
+        if (!hiddenPostIds.docs && !unfollowedUserIds.docs && !displayedPosts.docs.length) return
+
+        const temp = {docs: []}
+        temp.docs = displayedPosts.docs
+            .filter(doc => !unfollowedUserIds?.includes(doc.data().userId))
+            .filter(doc => !hiddenPostIds?.includes(doc.id))
+        setDisplayedPosts(temp)
+    }, [hiddenPostIds, unfollowedUserIds])
+
     return (
         <div>
-            {posts && posts.docs
-                .sort((a, b) => b.data().createdAt.seconds - a.data().createdAt.seconds)
-                .map(post => (
-                        <Post {...post.data()} key={post.id} postId={post.id}/>
-                    )
-                )}
+            {loadingSubs
+                ? <p>Paw...Paw...Paw...Paw...</p>
+                : displayedPosts.docs.length
+                    ? displayedPosts.docs
+                        .sort((a, b) => b.data().createdAt.seconds - a.data().createdAt.seconds)
+                        .map(post => (
+                                <Post {...post.data()} key={post.id} postId={post.id}/>
+                        ))
+                    : <p>No posts</p>
+            }
         </div>
     );
 }
